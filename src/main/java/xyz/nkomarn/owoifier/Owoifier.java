c94d4afe07ab85a17c68dc3330f77dbd38ea54b4
Mykyta <nkomarn@hotmail.com>
2020-04-14 14:14:08 -0700

    📐 owoify sign text as well!

--- src/main/java/xyz/nkomarn/owoifier/Owoifier.java
+++ src/main/java/xyz/nkomarn/owoifier/Owoifier.java
@@ -2,6 +2,7 @@ package xyz.nkomarn.owoifier;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,8 +26,20 @@ public class Owoifier extends JavaPlugin implements Listener {
     public void onAsyncChat(AsyncPlayerChatEvent event) {
         final Random random = new Random();
         final String expression = expressions[random.nextInt(expressions.length)];
-        event.setMessage(event.getMessage().replace("l", "w").replace("L", "W")
+        event.setMessage(owoify(event.getMessage()) + " " + expression);
+    }
+
+    @EventHandler
+    public void onSignChange(SignChangeEvent event) {
+        int lineNumber = 0;
+        for (final String line : event.getLines()) {
+             event.setLine(lineNumber++, owoify(line));
+        }
+    }
+
+    private String owoify(final String text) {
+        return text.replace("l", "w").replace("L", "W")
                 .replace("r", "w").replace("R", "W").replace("o", "u")
-                .replace("O", "U") + " " + expression);
+                .replace("O", "U");
     }
 }
